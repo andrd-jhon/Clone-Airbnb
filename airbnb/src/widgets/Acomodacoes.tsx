a0879c68e09d67bc5867c82ad72aab8b6dbf8b8a
andrd-jhon <jv-andrade02@outlook.com>
2026-01-25 19:40:26 -0300

    Transformando item inteiro em link.

--- airbnb/src/widgets/Acomodacoes.tsx
+++ airbnb/src/widgets/Acomodacoes.tsx
@@ -1,5 +1,6 @@
 import Acomodacao from '@/components/Acomodacao/Acomodacao'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const acomodacoes = [
     {
@@ -587,7 +588,8 @@ const Acomodacoes = () => {
         <section className='py-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {acomodacoes.map((acomodacao, indice) => (
                 <div key={indice}>
-                    <Acomodacao 
+                    <Link href={acomodacao.slug}>
+                      <Acomodacao 
                       local={acomodacao.location.description}
                       anfitriao={acomodacao.host}
                       data={acomodacao.date}
@@ -601,7 +603,8 @@ const Acomodacoes = () => {
                         width={300}
                         height={300}>
                         </Image>
-                    </Acomodacao>
+                    </Acomodacao>w
+                    </Link>
                 </div>
             ))}
         </section>
