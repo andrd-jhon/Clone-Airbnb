10472ab947c2425c0291080d745a84476113b08f
andrd-jhon <jv-andrade02@outlook.com>
2026-02-18 21:37:51 -0300

    Conclusão widget de descrição do imóvel.

--- airbnb/src/assets/icones.ts
+++ airbnb/src/assets/icones.ts
@@ -3,6 +3,16 @@ export {
     IconSearch, 
     IconAdjustmentsHorizontal, 
     IconHeartFilled,
-    IconStarFilled
+    IconStarFilled,
+    IconPhoto,
+    IconToolsKitchen2,
+    IconDesk,
+    IconPool,
+    IconBrandCarbon,
+    IconSailboat,
+    IconWifi,
+    IconParking,
+    IconAlarmSmoke,
+    IconDog
 } from '@tabler/icons-react'
 
